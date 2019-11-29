b1db4e263b4180cf2fe253b35eeb5e36542891b8
Kosuke Kito <blueberryjam09@gmail.com>
2019-11-29 14:10:24 +0900

    Add scroll_to_index handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,18 +26,10 @@ class NavigationPage extends StatefulWidget {
 }
 
 class _NavigationPageState extends State<NavigationPage> {
-  int _selectedIndex = 0;
-  int _scrollIndex = 0;
-
   static const List<String> _titles = [
     'scroll_to_index',
     'indexed_list_view',
     'scrollable_positioned_list',
-  ];
-  static const List<Widget> _pages = [
-    _ScrollToIndexList(),
-    _ScrollToIndexList(),
-    _ScrollToIndexList(),
   ];
   static const List<IconData> _icons = [
     Icons.home,
@@ -44,8 +37,21 @@ class _NavigationPageState extends State<NavigationPage> {
     Icons.search,
   ];
 
+  final _autoScrollController = AutoScrollController(
+    axis: Axis.vertical,
+    suggestedRowHeight: 210.0,
+  );
+
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    final List<Widget> _pages = [
+      _ScrollToIndexList(_autoScrollController),
+      _IndexedListViewList(),
+      _ScrollablePositionedList(),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text(_titles[_selectedIndex]),
@@ -75,20 +81,100 @@ class _NavigationPageState extends State<NavigationPage> {
             ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _scrollIndex = Random().nextInt(300);
-          });
+      floatingActionButton: Builder(
+        builder: (context) {
+          return FloatingActionButton(
+            onPressed: () async {
+              final index = Random().nextInt(300);
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(index.toString()),
+                  duration: const Duration(milliseconds: 200),
+                ),
+              );
+              final oo = await _autoScrollController.scrollToIndex(
+                index,
+                preferPosition: AutoScrollPosition.begin,
+              );
+              print(oo);
+            },
+            child: Icon(Icons.arrow_downward),
+          );
         },
-        child: Icon(Icons.arrow_downward),
       ),
     );
   }
 }
 
 class _ScrollToIndexList extends StatelessWidget {
-  const _ScrollToIndexList();
+  _ScrollToIndexList(this.controller);
+
+  final AutoScrollController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return AutoScrollTag(
+          key: ValueKey(index),
+          controller: controller,
+          index: index,
+          child: Container(
+            height: (Random().nextInt(400) + 20).toDouble(),
+            color: Color.fromRGBO(
+              Random().nextInt(256),
+              Random().nextInt(256),
+              Random().nextInt(256),
+              1.0,
+            ),
+            child: Center(
+              child: Text(
+                index.toString(),
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _IndexedListViewList extends StatelessWidget {
+  const _IndexedListViewList();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return Container(
+          height: (Random().nextInt(400) + 20).toDouble(),
+          color: Color.fromRGBO(
+            Random().nextInt(256),
+            Random().nextInt(256),
+            Random().nextInt(256),
+            1.0,
+          ),
+          child: Center(
+            child: Text(
+              index.toString(),
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16.0,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _ScrollablePositionedList extends StatelessWidget {
+  const _ScrollablePositionedList();
 
   @override
   Widget build(BuildContext context) {

a8c0c78214d12d9e4566d3d2a661812161a08d97
Kosuke Kito <blueberryjam09@gmail.com>
2019-11-29 11:01:28 +0900

    Add the project

---
+++ android/app/src/main/kotlin/kitoko552/com/scroll_to_index_list/MainActivity.kt
@@ -0,0 +1,13 @@
+package kitoko552.com.scroll_to_index_list
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
